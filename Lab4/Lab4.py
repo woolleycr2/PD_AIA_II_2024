96a0e341186457ca44ed5c1107d7215909cca0eb
Ionaşcu Iulian-Aurelian <ii231@student.ugal.ro>
2024-12-01 22:53:29 +0200

    rezolvat 8 si 9 din Lab4

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #1
-df1 = pd.read_csv('date_climatice_2016.csv')
+df1 = pd.read_csv("date_climatice_2016.csv")
 print(df1)
 
 #2
@@ -16,14 +16,14 @@ df1 = df1.astype({
 df1.info()
 
 #4
-df2 = pd.read_csv('statii_meteo.csv')
+df2 = pd.read_csv("statii_meteo.csv")
 print(df2)
 
 #5
 df2 = df2.rename(columns={"ID": "CODST", "Nume": "NUME"})
 
 #6
-df1 = df2.merge(df1, on='CODST')
+df1 = df2.merge(df1, on="CODST")
 
 #7
 mean_TMED = df1["TMED"].mean()
@@ -33,6 +33,17 @@ print(max_TMAX, df1.query("TMAX == @max_TMAX")["NUME"])
 min_TMIN = df1["TMIN"].min()
 print(min_TMIN, df1.query("TMIN == @min_TMIN")["NUME"])
 
-#8
+#8a
+date_statistice = df1[["NUME", "ALT", "TMIN"]].groupby("NUME").min()
+
+#8b
+date_statistice = date_statistice.merge(df1[["NUME", "TMED"]].groupby("NUME").mean(), on="NUME")
+
+#8c
+date_statistice = date_statistice.merge(df1[["NUME", "TMAX"]].groupby("NUME").max(), on="NUME")
+
+#8d
+print(date_statistice)
 
 #9
+print(date_statistice.corr())
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-WIP:   
-Lab4 - 8, 9  
+WIP:
 Lab5 - 3b, 3c  
 Lab6 - 3, 4   
 Lab7  
